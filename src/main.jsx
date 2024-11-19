9ed9f2a648f243bef95c24c3b96eed78460d1300
jiwonee02 <nepthun@naver.com>
2024-11-19 16:48:31 +0900

    🎉 Start : pwa 세팅
    pwa 세팅

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
