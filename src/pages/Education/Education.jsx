c124e30f8633969b805a20641aa54085c7a70fd1
Your Name <zmarzmzm@naver.com>
2024-11-19 12:27:38 +0900

    🎉Start: 초기 설정

---
+++ src/pages/Education/Education.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import * as C from "../../styles/CommonStyle";
+
+function Education() {
+  return (
+    <C.Page>
+      <C.Center>
+        <C.PageSpace></C.PageSpace>
+      </C.Center>
+    </C.Page>
+  );
+}
+
+export default Education;
---
+++ src/pages/Login/Select.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import * as C from "../../styles/CommonStyle";
+
+function Select() {
+  return (
+    <C.Page>
+      <C.Center>
+        <C.PageSpace></C.PageSpace>
+      </C.Center>
+    </C.Page>
+  );
+}
+
+export default Select;
