607e7037f596119c75fdeadd30d39fa7b0f24b07
jiwonee02 <nepthun@naver.com>
2024-11-19 17:21:47 +0900

    ✨ Feat : 게시판 페이지 구현
    게시판 페이지 구현

---
+++ src/components/Notice.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import * as N from "../styles/Components/NoticeStyle";
+import More from "../assets/images/Common/more.png";
+import Test from "../assets/images/Common/test.png";
+
+const Notice = () => {
+  return (
+    <N.List>
+      <N.ListItem>
+        <N.ListImg src={Test} alt="test"></N.ListImg>
+        <N.ListText>
+          <N.ListTitle>Test</N.ListTitle>
+          <N.ListMore src={More} alt="more"></N.ListMore>
+        </N.ListText>
+      </N.ListItem>
+      <N.ListItem>
+        <N.ListImg src={Test} alt="test"></N.ListImg>
+        <N.ListText>
+          <N.ListTitle>Test</N.ListTitle>
+          <N.ListMore src={More} alt="more"></N.ListMore>
+        </N.ListText>
+      </N.ListItem>
+    </N.List>
+  );
+};
+
+export default Notice;
