85a5dbd11f801bb816a927e0bbbda9388cf46801
Dongmyoung Min <zmar@Dongmyoungui-MacBookAir.local>
2024-11-19 18:06:58 +0900

    ✨Feat: 로그인 첫 페이지

--- src/styles/Login/FirstPageStyle.jsx
+++ src/styles/Login/FirstPageStyle.jsx
@@ -1,32 +1,61 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-import * as C from "../../styles/CommonStyle";
-import * as F from "../../styles/Login/FirstPageStyle";
-import Anyone from "../../assets/images/Login/Anyone.png";
+import styled from "styled-components";
 
-function FirstPage() {
-  const navigate = useNavigate();
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 55%; 
+  width: 100%;
+`;
 
-  return (
-    <C.Page>
-      <C.Center>
-        <C.PageSpace>
-          <F.Wrapper>
-            <F.Image src={Anyone} alt="Anyone" />
-            <F.Comment>로그인을 해주세요</F.Comment>
-            <F.ButtonContainer>
-              <F.Button className="signup" onClick={() => navigate("/signup")}>
-                회원가입
-              </F.Button>
-              <F.Button className="login" onClick={() => navigate("/signin")}>
-                로그인
-              </F.Button>
-            </F.ButtonContainer>
-          </F.Wrapper>
-        </C.PageSpace>
-      </C.Center>
-    </C.Page>
-  );
-}
+export const ButtonContainer = styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+`;
 
-export default FirstPage;
+export const Button = styled.button`
+  width: 344px;
+  height: 50px;
+  font-size: 16px;
+  font-weight: bold;
+  border: none;
+  border-radius: 30px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+
+  &.signup {
+    background-color: #EE8814; 
+    color: white;
+  }
+
+  &.signup:hover {
+    background-color: #FFFFFF; 
+  }
+
+  &.login {
+    background-color: #EE8814;
+    color: white;
+    margin-top: 20px;
+  }
+
+  &.login:hover {
+    background-color: #FFFFFF; 
+  }
+`;
+
+export const Image = styled.img`
+  width: 175px;
+  height: 37px;
+  display: block;
+  margin: 0 auto;
+`;
+
+export const Comment = styled.p`
+  font-size: 14px;
+  color: #666;
+  text-align: center;
+  margin-top: 20px;
+  margin-bottom: 65px;
+`;  
+
