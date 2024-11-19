0c655e1a45395c51bb95c915ff38d3c8027a1bd6
jiwonee02 <nepthun@naver.com>
2024-11-20 00:04:34 +0900

    ✨ Feat : 검색 페이지 구현
    검색 페이지 css 구현

--- src/pages/Education/SearchE.jsx
+++ src/pages/Education/SearchE.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 import * as C from "../../styles/CommonStyle";
+import * as SE from "../../styles/Education/SearchEStyle";
+import Delete from "../../assets/images/Common/delete.png";
 
 function SearchE() {
   return (
     <C.Page>
       <C.Center>
-        <C.PageSpace></C.PageSpace>
+        <C.PageSpace>
+          <SE.Search>
+            <SE.SearchTitle>최근 검색</SE.SearchTitle>
+            <SE.SearchContent>
+              <SE.SearchItem>
+                <SE.SearchText>뜨개 키링 만들기</SE.SearchText>
+                <SE.SearchDelete src={Delete} alt="Delete"></SE.SearchDelete>
+              </SE.SearchItem>
+            </SE.SearchContent>
+          </SE.Search>
+        </C.PageSpace>
       </C.Center>
     </C.Page>
   );
