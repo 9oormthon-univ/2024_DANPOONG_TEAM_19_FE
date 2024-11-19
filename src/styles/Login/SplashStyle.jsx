545db4e8a012e408b05b7b2fe68eac3cb5b577fa
Dongmyoung Min <zmar@Dongmyoungui-MacBookAir.local>
2024-11-19 18:05:27 +0900

    ✨Feat: Splash 구현

---
+++ src/styles/Login/SplashStyle.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const ImageContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: width: 100vw;
+  height: 100dvh;
+`;
+
+export const Image = styled.img`
+  width:100vw;
+  height:100dvh;
+`;
