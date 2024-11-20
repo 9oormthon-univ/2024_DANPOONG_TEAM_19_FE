676da7a48f9ccfd2c5338c4cbdf1c55931fb2f4f
jiwonee02 <nepthun@naver.com>
2024-11-20 14:37:11 +0900

    ✨ Feat : 구매자 상품관리 모달 구햔
    구매자 상품관리 모달 css 구현

--- src/components/MyPageB.jsx
+++ src/components/MyPageB.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import * as MB from "../styles/Components/MyPageBStyle";
 import Test from "../assets/images/Common/test.png";
 import Plus from "../assets/images/MyPage/plus.png";
+import ModalProgressB from "../components/ModalProgressB";
 
 function MypageB() {
+  const [isModalOpenP, setIsModalOpenP] = useState(false);
+  const [isModalVisibleP, setIsModalVisibleP] = useState(false);
+
+  const openModalP = () => {
+    setIsModalOpenP(true);
+    setIsModalVisibleP(true);
+  };
+  const closeModalP = () => {
+    setIsModalVisibleP(false);
+    setTimeout(() => {
+      setIsModalOpenP(false);
+    }, 400);
+  };
+
   return (
     <MB.List>
       <MB.ListItem>
@@ -12,7 +27,8 @@ function MypageB() {
             <MB.ListImg src={Test}></MB.ListImg>
             <MB.ListText>Test</MB.ListText>
           </MB.ListContainer>
-          <MB.ListMore src={Plus}></MB.ListMore>
+          <MB.ListMore src={Plus} onClick={openModalP}></MB.ListMore>
+          {isModalOpenP && <ModalProgressB onClose={closeModalP} isModalVisibleP={isModalVisibleP} />}
         </MB.ListContent>
       </MB.ListItem>
     </MB.List>
