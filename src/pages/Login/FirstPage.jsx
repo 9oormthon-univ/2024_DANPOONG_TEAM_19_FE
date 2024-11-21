8e665128f065e24dc2b98d0e1e9a3bcd67b16ecf
Your Name <zmarzmzm@naver.com>
2024-11-21 11:25:29 +0900

    ✨Feat: 회원가입 로그인

--- src/pages/Login/FirstPage.jsx
+++ src/pages/Login/FirstPage.jsx
@@ -1,11 +1,29 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import * as C from "../../styles/CommonStyle";
+import * as F from "../../styles/Login/FirstPageStyle";
+import Anyone from "../../assets/images/Login/Anyone.png";
 
 function FirstPage() {
+  const navigate = useNavigate();
+
   return (
     <C.Page>
       <C.Center>
-        <C.PageSpace></C.PageSpace>
+        <C.PageSpace>
+          <F.Wrapper>
+            <F.Image src={Anyone} alt="Anyone" />
+            <F.Comment>로그인을 해주세요</F.Comment>
+            <F.ButtonContainer>
+              <F.Button className="signup" onClick={() => navigate("/signup")}>
+                회원가입
+              </F.Button>
+              <F.Button className="login" onClick={() => navigate("/signin")}>
+                로그인
+              </F.Button>
+            </F.ButtonContainer>
+          </F.Wrapper>
+        </C.PageSpace>
       </C.Center>
     </C.Page>
   );
