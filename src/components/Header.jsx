62e344bf37185e9d5f869a0169e3b29195ea3d42
Your Name <zmarzmzm@naver.com>
2024-11-21 23:07:17 +0900

    🔄️Rename: 대소문자   변경

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import * as H from "../styles/Components/HeaderStyle";
-import anyone from "../assets/images/Header/anyone.png";
+import anyone from "../assets/images/Header/Anyone.png";
 import search from "../assets/images/Header/search.svg";
 import alarm from "../assets/images/Header/alarm.svg";
 
