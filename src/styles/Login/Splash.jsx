75a6e3e3556bc4fe148e883ae4fbcba7245685cb
Dongmyoung Min <zmar@Dongmyoungui-MacBookAir.local>
2024-11-22 12:27:49 +0900

    🐛Fix: Splash  수정

--- src/styles/Login/Splash.jsx
+++ src/styles/Login/Splash.jsx
@@ -1,5 +1,29 @@
 import styled from "styled-components";
 
+export const Page = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  overflow: hidden;
+  position: relative;
+`;
+
+export const Center = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const PageSpace = styled.div`
+  width: 100vw;
+  height: 100vh;
+`;
+
 export const ImageContainer = styled.div`
   display: flex;
   justify-content: center;
