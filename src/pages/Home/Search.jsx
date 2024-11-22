8db261a5b73dcb19cbf5fb28c4cd55d42c83a28e
jiwonee02 <nepthun@naver.com>
2024-11-22 17:56:26 +0900

    🎨 Design : 헤더 추가
    헤더 추가

--- src/pages/Home/Search.jsx
+++ src/pages/Home/Search.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import * as C from "../../styles/CommonStyle";
 import * as S from "../../styles/Home/SearchStyle";
 import Delete from "../../assets/images/Common/delete.png";
+import SearchHeader from "../../components/searchHeader";
 
 function SearchE() {
   return (
     <C.Page>
       <C.Center>
         <C.PageSpace>
+          <SearchHeader />
           <S.Search>
             <S.SearchTitle>최근 검색</S.SearchTitle>
             <S.SearchContent>
