d6b67e3dd75c74b3a37699644c9ee772fb77db45
Dongmyoung Min <zmar@Dongmyoungui-MacBookAir.local>
2024-11-22 12:37:26 +0900

    🐛Fix: Splash  수정

--- src/pages/Login/Splash.jsx
+++ src/pages/Login/Splash.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import * as C from "../../styles/CommonStyle";
 import * as S from "../../styles/Login/Splash";
 import Loading from "../../assets/images/Login/loading.png";
 
@@ -16,15 +15,9 @@ function Splash() {
   }, [navigate]);
 
   return (
-    <S.Page>
-      <S.Center>
-        <S.PageSpace>
-          <S.ImageContainer>
-            <S.Image src={Loading} alt="Loading" />
-          </S.ImageContainer>
-        </S.PageSpace>
-      </S.Center>
-    </S.Page>
+    <S.ImageContainer>
+      <S.Image src={Loading} alt="Loading" />
+    </S.ImageContainer>
   );
 }
 
