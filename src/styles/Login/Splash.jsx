6b258bb32d950408442c51dbc80220d6a42e8985
Dongmyoung Min <zmar@Dongmyoungui-MacBookAir.local>
2024-11-22 12:11:24 +0900

    🐛Fix: Splash 이미지 크기 수정

--- src/styles/Login/Splash.jsx
+++ src/styles/Login/Splash.jsx
@@ -4,11 +4,13 @@ export const ImageContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: width: 100vw;
+  width: 100vw;
   height: 100dvh;
 `;
 
 export const Image = styled.img`
-  width:100vw;
-  height:100dvh;
+  display: block; 
+  width: 100vw;
+  height: 100dvh;
+  object-fit: cover; 
 `;
