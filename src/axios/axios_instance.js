ba02a487d51145a47e3de53a3095d27755976aff
jiwonee02 <nepthun@naver.com>
2024-11-22 22:12:15 +0900

    🔥 Remove : env 파일 삭제
    env 파일 삭제

--- src/axios/axios_instance.js
+++ src/axios/axios_instance.js
@@ -1,11 +1,19 @@
 import axios from "axios";
 
-const token = import.meta.env.VITE_TOKEN;
-
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_SERVER_URL,
   timeout: 5000,
-  headers: {
-    Authorization: `Bearer ${token}`,
-  },
 });
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
