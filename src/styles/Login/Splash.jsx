bc1f8b547024e3a23651233f9ad48c29ebec7f27
Your Name <zmarzmzm@naver.com>
2024-11-22 12:37:26 +0900

    🐛Fix: Splash  수정

--- src/styles/Login/Splash.jsx
+++ src/styles/Login/Splash.jsx
@@ -1,29 +1,5 @@
 import styled from "styled-components";
 
-export const Page = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100vw;
-  height: 100vh;
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
-  overflow: hidden;
-  position: relative;
-`;
-
-export const Center = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-export const PageSpace = styled.div`
-  width: 100vw;
-  height: 100vh;
-`;
-
 export const ImageContainer = styled.div`
   display: flex;
   justify-content: center;
@@ -33,7 +9,6 @@ export const ImageContainer = styled.div`
 `;
 
 export const Image = styled.img`
-  display: block; 
   width: 100vw;
   height: 100dvh;
   object-fit: cover; 
